383a63b86eb00a1a08585d0ef3f88989d910d32f
ramblehead <vrybynok@cambustion.com>
2016-03-29 09:51:18 +0100

    Conditional C++11 patch for emacs <25

--- init.el
+++ init.el
@@ -773,68 +773,71 @@ fields which we need."
 ;; /usr/include/x86_64-linux-gnu
 ;; /usr/include")))))
 
+;; The following patch is only required for emacs < 25.
+;; Once transition to emacs 25 is completed, this patch should be removed.
 ;; This patch if far from perfect. It is although better than nothing.
 ;; see http://stackoverflow.com/questions/8549351/c11-mode-or-settings-for-emacs
-;; (defun vr-c++-11-partial-patch ()
-;;   (require 'font-lock)
-;;   (defun --copy-face (new-face face)
-;;     "Define NEW-FACE from existing FACE."
-;;     (copy-face face new-face)
-;;     (eval `(defvar ,new-face nil))
-;;     (set new-face new-face))
-;;   ;; labels, case, public, private, proteced, namespace-tags
-;;   (--copy-face 'font-lock-label-face
-;;                'font-lock-keyword-face)
-;;   ;; comment markups such as Javadoc-tags
-;;   (--copy-face 'font-lock-doc-markup-face
-;;                'font-lock-doc-face)
-;;   ;; comment markups
-;;   (--copy-face 'font-lock-doc-string-face
-;;                'font-lock-comment-face)
-;;   (global-font-lock-mode t)
-;;   (setq font-lock-maximum-decoration t)
-;;   ;; We could place some regexes into `c-mode-common-hook', but note that their evaluation order
-;;   ;; matters.
-;;   (font-lock-add-keywords
-;;    nil '(;; complete some fundamental keywords
-;;          ("\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long\\|float\\|double\\)\\>" . font-lock-keyword-face)
-;;          ;; namespace names and tags - these are rendered as constants by cc-mode
-;;          ("\\<\\(\\w+::\\)" . font-lock-function-name-face)
-;;          ;;  new C++11 keywords
-;;          ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" 1 font-lock-keyword-face)
-;;          ("\\<\\(char16_t\\|char32_t\\)\\>" . font-lock-keyword-face)
-;;          ;; PREPROCESSOR_CONSTANT, PREPROCESSORCONSTANT
-;;          ("\\<[A-Z]*_[A-Z_]+\\>" . font-lock-constant-face)
-;;          ("\\<[A-Z]\\{3,\\}\\>"  . font-lock-constant-face)
-;;          ;; hexadecimal numbers
-;;          ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
-;;          ;; integer/float/scientific numbers
-;;          ;; ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>" . font-lock-constant-face)
-;;          ;; integer/float/scientific literals (improved)
-;;          ("\\<[-+]?[0-9]*\\.?[0-9]+\\([uUlL]+\\|[eE][-+]?[0-9]+\\)?[fFlL]?\\>" . font-lock-constant-face)
-;;          ;; c++11 string literals
-;;          ;;       L"wide string"
-;;          ;;       L"wide string with UNICODE codepoint: \u2018"
-;;          ;;       u8"UTF-8 string", u"UTF-16 string", U"UTF-32 string"
-;;          ("\\<\\([LuU8]+\\)\".*?\"" 1 font-lock-keyword-face)
-;;          ;;       R"(user-defined literal)"
-;;          ;;       R"( a "quot'd" string )"
-;;          ;;       R"delimiter(The String Data" )delimiter"
-;;          ;;       R"delimiter((a-z))delimiter" is equivalent to "(a-z)"
+(defun vr-c++-11-partial-patch ()
+  (message "vr-c++-11-partial-patch")
+  (require 'font-lock)
+  (defun --copy-face (new-face face)
+    "Define NEW-FACE from existing FACE."
+    (copy-face face new-face)
+    (eval `(defvar ,new-face nil))
+    (set new-face new-face))
+  ;; labels, case, public, private, proteced, namespace-tags
+  (--copy-face 'font-lock-label-face
+               'font-lock-keyword-face)
+  ;; comment markups such as Javadoc-tags
+  (--copy-face 'font-lock-doc-markup-face
+               'font-lock-doc-face)
+  ;; comment markups
+  (--copy-face 'font-lock-doc-string-face
+               'font-lock-comment-face)
+  (global-font-lock-mode t)
+  (setq font-lock-maximum-decoration t)
+  ;; We could place some regexes into `c-mode-common-hook', but note that their evaluation order
+  ;; matters.
+  (font-lock-add-keywords
+   nil '(;; complete some fundamental keywords
+         ("\\<\\(void\\|unsigned\\|signed\\|char\\|short\\|bool\\|int\\|long\\|float\\|double\\)\\>" . font-lock-keyword-face)
+         ;; namespace names and tags - these are rendered as constants by cc-mode
+         ("\\<\\(\\w+::\\)" . font-lock-function-name-face)
+         ;;  new C++11 keywords
+         ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" 1 font-lock-keyword-face)
+         ("\\<\\(char16_t\\|char32_t\\)\\>" . font-lock-keyword-face)
+         ;; PREPROCESSOR_CONSTANT, PREPROCESSORCONSTANT
+         ("\\<[A-Z]*_[A-Z_]+\\>" . font-lock-constant-face)
+         ("\\<[A-Z]\\{3,\\}\\>"  . font-lock-constant-face)
+         ;; hexadecimal numbers
+         ("\\<0[xX][0-9A-Fa-f]+\\>" . font-lock-constant-face)
+         ;; integer/float/scientific numbers
+         ;; ("\\<[\\-+]*[0-9]*\\.?[0-9]+\\([ulUL]+\\|[eE][\\-+]?[0-9]+\\)?\\>" . font-lock-constant-face)
+         ;; integer/float/scientific literals (improved)
+         ("\\<[-+]?[0-9]*\\.?[0-9]+\\([uUlL]+\\|[eE][-+]?[0-9]+\\)?[fFlL]?\\>" . font-lock-constant-face)
+         ;; c++11 string literals
+         ;;       L"wide string"
+         ;;       L"wide string with UNICODE codepoint: \u2018"
+         ;;       u8"UTF-8 string", u"UTF-16 string", U"UTF-32 string"
+         ("\\<\\([LuU8]+\\)\".*?\"" 1 font-lock-keyword-face)
+         ;;       R"(user-defined literal)"
+         ;;       R"( a "quot'd" string )"
+         ;;       R"delimiter(The String Data" )delimiter"
+         ;;       R"delimiter((a-z))delimiter" is equivalent to "(a-z)"
 
-;;          ;; ("\\(\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\)[[:ascii:][:nonascii:]]*?\\()[^\\s-\\\\()]\\{0,16\\}\"\\)" 0 font-lock-keyword-face t) ; start/end delimiter
-;;          ;; (   "\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\([[:ascii:][:nonascii:]]*?\\))[^\\s-\\\\()]\\{0,16\\}\"" 1 font-lock-string-face t)  ; actual string
+         ;; ("\\(\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\)[[:ascii:][:nonascii:]]*?\\()[^\\s-\\\\()]\\{0,16\\}\"\\)" 0 font-lock-keyword-face t) ; start/end delimiter
+         ;; (   "\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\([[:ascii:][:nonascii:]]*?\\))[^\\s-\\\\()]\\{0,16\\}\"" 1 font-lock-string-face t)  ; actual string
 
-;;          ("\\(\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\)" 1 font-lock-keyword-face t) ; start delimiter
-;;          (   "\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\([[:ascii:][:nonascii:]]*?\\))[^\\s-\\\\()]\\{0,16\\}\"" 1 font-lock-string-face prepend)  ; actual string
-;;          (   "\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}([[:ascii:][:nonascii:]]*?\\()[^\\s-\\\\()]\\{0,16\\}\"\\)" 1 font-lock-keyword-face t) ; end delimiter
+         ("\\(\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\)" 1 font-lock-keyword-face t) ; start delimiter
+         (   "\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}(\\([[:ascii:][:nonascii:]]*?\\))[^\\s-\\\\()]\\{0,16\\}\"" 1 font-lock-string-face prepend)  ; actual string
+         (   "\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}([[:ascii:][:nonascii:]]*?\\()[^\\s-\\\\()]\\{0,16\\}\"\\)" 1 font-lock-keyword-face t) ; end delimiter
 
-;;          ;; ("\\(\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}([[:ascii:][:nonascii:]]*?)[^\\s-\\\\()]\\{0,16\\}\"\\)" 1 font-lock-string-face t)
+         ;; ("\\(\\<[uU8]*R\"[^\\s-\\\\()]\\{0,16\\}([[:ascii:][:nonascii:]]*?)[^\\s-\\\\()]\\{0,16\\}\"\\)" 1 font-lock-string-face t)
 
-;;          ;; user-defined types (rather project-specific)
-;;          ;; ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(type\\|ptr\\)\\>" . font-lock-type-face)
-;;          ;; ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)
-;;          ) t))
+         ;; user-defined types (rather project-specific)
+         ;; ("\\<[A-Za-z_]+[A-Za-z_0-9]*_\\(type\\|ptr\\)\\>" . font-lock-type-face)
+         ;; ("\\<\\(xstring\\|xchar\\)\\>" . font-lock-type-face)
+         ) t))
 
 ;; TODO: make all auto-complete settings buffer local
 (defun vr-c++-clang-auto-complete-setup ()
@@ -1090,7 +1093,8 @@ fields which we need."
                   ;; (google-set-c-style)
                   (vr-c++-indentation-setup)
                   (vr-c++-yas-setup)
-                  ;; (vr-c++-11-partial-patch)
+                  (if (< (car vr-emacs-version) 25)
+                      (vr-c++-11-partial-patch))
                   (vr-c++-clang-auto-complete-setup)
                   (vr-c++-compile-setup)
                   (vr-c++-debug-setup)
