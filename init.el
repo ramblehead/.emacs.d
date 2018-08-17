131d426da7d2972ebfa6502d3162186387c9b3cd
ramblehead <v.rybynok@gmail.com>
2018-08-17 22:57:56 +0100

    Fixing YAS symbol height with "Noto Sans Tifinagh-9"
    Using this simbol (ⵙ) for yas mode :)

--- init.el
+++ init.el
@@ -847,6 +847,7 @@ code-groups minor mode - i.e. the function usually bound to C-M-n")
 
     ;; see https://github.com/shosti/.emacs.d/blob/master/personal/p-display.el#L9
     (set-fontset-font t (decode-char 'ucs #x2d5b) "Noto Sans Tifinagh-9") ; ⵛ
+    (set-fontset-font t (decode-char 'ucs #x2d59) "Noto Sans Tifinagh-9") ; ⵙ
     (set-fontset-font t (decode-char 'ucs #x2b6f) "Symbola-9.5") ; ⭯
     (set-fontset-font t (decode-char 'ucs #x2b73) "Symbola-9.5") ; ⭳
     )
