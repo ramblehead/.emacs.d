1fc5d8c0774d898c395b113e44c2c35d61fe3d7b
ramblehead <v.rybynok@gmail.com>
2018-12-17 23:09:04 +0000

    Adding (require 'js2-mode) to rh-scratch-js pseudo-mode

--- lisp/rh-scratch-js.el
+++ lisp/rh-scratch-js.el
@@ -1,6 +1,7 @@
 ;;; ramblehead's rh-scratch-js mode
 
 (require 'nodejs-repl)
+(require 'js2-mode)
 
 ;;;###autoload
 (defun rh-scratch-js ()
@@ -13,7 +14,7 @@
        (setq buf (get-buffer-create "*rh-scratch-js*")))
      (with-current-buffer buf
        (js2-mode)
-       (rh-nodejs-interaction)
+       (rh-nodejs-interaction 1)
        (set (make-local-variable 'js2-strict-missing-semi-warning) nil)
        (unless bufp
          (insert "// This buffer is for text that is not saved, and for JavaScript evaluation.\n\n")))
