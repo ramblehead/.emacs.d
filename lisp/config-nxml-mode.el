ebd667e8acdc182c432c4e5e1f45edf25eed4ece
ramblehead <v.rybynok@gmail.com>
2019-06-10 21:11:59 +0100

    Checkpoint

--- lisp/config-nxml-mode.el
+++ lisp/config-nxml-mode.el
@@ -27,28 +27,63 @@ For example:
     </img>
   </message>
 "
-  (let (prev-bol ref-column looking-at->)
-    (catch 'indent
-      (save-excursion
-	;; Move backwards until the start of a non-blank line that is
-	;; not inside a token.
-	(while (progn
-		 (when (= (forward-line -1) -1)
-		   (throw 'indent 0))
-		 (back-to-indentation)
-		 (if (looking-at "[ \t]*$")
-		     t
-		   (or prev-bol
-		       (setq prev-bol (point)))
-		   (nxml-token-after)
-		   (not (or (= xmltok-start (point))
-			    (eq xmltok-type 'data))))))
-        (setq ref-column (current-column)))
-      (save-excursion
-        (forward-line 0)
-        (skip-chars-forward " \t")
-        (setq looking-at-> (looking-at ">")))
-      (if looking-at-> ref-column (funcall orig-fun)))))
+  (let (prev-bol looking-at->)
+    (save-excursion
+      (forward-line 0)
+      (skip-chars-forward " \t")
+      (setq looking-at-> (looking-at ">")))
+    (if looking-at->
+        (save-excursion
+          ;; Move backwards until the start of a non-blank line that is
+          ;; not inside a token.
+          (while (progn
+                   (forward-line -1)
+                   (back-to-indentation)
+                   (if (looking-at "[ \t]*$")
+                       t
+                     (or prev-bol
+                         (setq prev-bol (point)))
+                     (nxml-token-after)
+                     (not (or (= xmltok-start (point))
+                              (eq xmltok-type 'data))))))
+          (current-column))
+      (funcall orig-fun))))
+
+;; (defun rh-nxml-compute-indent-from-previous-line (orig-fun)
+;;   "React-like tag attributes closing pointy-bracket indentation.
+
+;; For example:
+;;   <message id=\"DevicePanelLogo\">
+;;     <img
+;;       src=\"img/cambustion_logo.png\"
+;;       alt=\"Cambustion\"
+;;       class=\"img-responsive\"
+;;     >
+;;     </img>
+;;   </message>
+;; "
+;;   (let (ref-column prev-bol looking-at->)
+;;     (catch 'indent
+;;       (save-excursion
+;;         (forward-line 0)
+;;         (skip-chars-forward " \t")
+;;         (setq looking-at-> (looking-at ">")))
+;;       (save-excursion
+;; 	;; Move backwards until the start of a non-blank line that is
+;; 	;; not inside a token.
+;; 	(while (progn
+;; 		 (when (= (forward-line -1) -1)
+;; 		   (throw 'indent 0))
+;; 		 (back-to-indentation)
+;; 		 (if (looking-at "[ \t]*$")
+;; 		     t
+;; 		   (or prev-bol
+;; 		       (setq prev-bol (point)))
+;; 		   (nxml-token-after)
+;; 		   (not (or (= xmltok-start (point))
+;; 			    (eq xmltok-type 'data))))))
+;;         (setq ref-column (current-column)))
+;;       (if looking-at-> ref-column (funcall orig-fun)))))
 
 (advice-add 'nxml-compute-indent-from-previous-line :around
             #'rh-nxml-compute-indent-from-previous-line)
