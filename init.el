c0ca417b46aa28f441a71f98a641d7932a130af7
ramblehead <v.rybynok@gmail.com>
2023-05-15 18:09:29 +0100

    Checkpoint

--- init.el
+++ init.el
@@ -105,6 +105,14 @@
   :ensure t
   :demand t)
 
+(setq load-prefer-newer t)
+(add-to-list 'load-path rh-user-lisp-directory-path)
+(add-to-list 'load-path "/usr/share/emacs/site-lisp")
+(load rh-user-site-start-file-path nil t t)
+
+(dolist (file-path rh-site-start-file-paths)
+  (load file-path nil t t))
+
 (use-package emacs
   :config
   ;; No ceremony
@@ -114,6 +122,8 @@
   ;; Windows splitting
   (setq split-height-threshold nil)
   (setq split-width-threshold 170)
+
+  (defalias 'yes-or-no-p 'y-or-n-p)
 
   (when (display-graphic-p)
     ;; Change cursor type according to mode
@@ -219,112 +229,8 @@
 ;;; /b/; dired
 ;;; /b/{
 
-(defun rh-dired-find-file ()
-  (interactive)
-  (let* ((filename (dired-get-file-for-visit))
-         (filename-after
-          (if (string= (file-name-nondirectory filename) "..")
-              (dired-current-directory)
-            (concat (file-name-as-directory filename) "..")))
-         (find-file-respect-dired
-          (lambda (fn)
-            (if (= (length (get-buffer-window-list)) 1)
-                (find-alternate-file fn)
-              (bury-buffer)
-              (find-file fn)))))
-    (if (not (file-directory-p filename))
-        (funcall find-file-respect-dired filename)
-      (if (not (file-accessible-directory-p filename))
-          (error (format "Directory '%s' is not accessible" filename))
-        (funcall find-file-respect-dired filename))
-      (dired-goto-file filename-after)
-      (recenter))))
-
-(defun rh-dired-change-to-file (filename)
-  (interactive "FFind file: ")
-  (find-alternate-file filename))
-
-(defun rh-dired-ace-select-other-window ()
-  (interactive)
-  (let ((file-name (dired-get-file-for-visit)))
-    (when (ace-select-window)
-      (find-file file-name))))
-
-(defun rh-get-buffer-mode-window-list (&optional buffer-or-name mode all-frames)
-  (setq mode (or mode major-mode))
-  (let ((window-list '()))
-    (dolist (win (window-list-1 nil nil all-frames))
-      (when (eq
-             (with-selected-window win
-               major-mode)
-             mode)
-        (push win window-list)))
-    (nreverse window-list)))
-
-(defun rh-dired-select-next-dired-window ()
-  (interactive)
-  (let ((window-list (rh-get-buffer-mode-window-list)))
-    (if (> (length window-list) 1)
-        (select-window (nth 1 window-list))
-      (let ((directory (dired-current-directory))
-            (pos (point)))
-        (when (ace-select-window)
-          (find-file directory)
-          (goto-char pos))))))
-
-(defun rh-dired-alt-ace-select-other-window ()
-  (interactive)
-  (with-selected-window (frame-selected-window)
-    (let ((name (dired-get-file-for-visit)))
-      (when (ace-select-window)
-        (find-file name)))))
-
-(defun rh-dired-change-to-parent-dir ()
-  (interactive)
-  (dired-goto-file (concat (dired-current-directory) ".."))
-  (rh-dired-find-file))
-
-(defun rh-dired-open-file ()
-  "In dired, open the file named on this line."
-  (interactive)
-  (let ((file (dired-get-filename nil t)))
-    (message "Opening %s..." file)
-    (call-process "xdg-open" nil 0 nil file)))
-
-;; (defun rh-dired-guess-dir ()
-;;   "Starts dired in buffer-file-name directory or in '~', if buffer has no
-;; filename associated with it."
-;;   (interactive)
-;;   (progn
-;;     (dired (let ((fnm (buffer-file-name)))
-;;              (if fnm
-;;                  (file-name-directory fnm)
-;;                "~")))))
-
 (use-package dired
-  :config
-  ;; copy from one dired dir to the next dired dir shown in a split window
-  (setq dired-dwim-target t)
-
-  (require 'ace-window)
-
-  (add-hook
-   'dired-mode-hook
-   (lambda ()
-     (hl-line-mode 1)
-     (setq-local coding-system-for-read vr-dired-coding-system)
-     (setq-local find-file-visit-truename nil)
-     ;; (add-hook
-     ;;  'window-selection-change-functions
-     ;;  (lambda (win)
-     ;;    (when (eq (with-selected-window win major-mode) 'dired-mode)
-     ;;      (if (eq win (frame-selected-window))
-     ;;          (hl-line-mode 1)
-     ;;        (with-selected-window win
-     ;;          (when (= (length (get-buffer-window-list)) 1)
-     ;;            (hl-line-mode -1))))))
-     ;;  nil t)
-     ))
+  :config (require 'config-dired)
 
   :bind
   (:map dired-mode-map
---
+++ lisp/config-dired.el
@@ -0,0 +1,97 @@
+;;; ramblehead's config-dired configuration
+
+(require 'ace-window)
+
+(defun rh-dired-find-file ()
+  (interactive)
+  (let* ((filename (dired-get-file-for-visit))
+         (filename-after
+          (if (string= (file-name-nondirectory filename) "..")
+              (dired-current-directory)
+            (concat (file-name-as-directory filename) "..")))
+         (find-file-respect-dired
+          (lambda (fn)
+            (if (= (length (get-buffer-window-list)) 1)
+                (find-alternate-file fn)
+              (bury-buffer)
+              (find-file fn)))))
+    (if (not (file-directory-p filename))
+        (funcall find-file-respect-dired filename)
+      (if (not (file-accessible-directory-p filename))
+          (error (format "Directory '%s' is not accessible" filename))
+        (funcall find-file-respect-dired filename))
+      (dired-goto-file filename-after)
+      (recenter))))
+
+(defun rh-dired-change-to-file (filename)
+  (interactive "FFind file: ")
+  (find-alternate-file filename))
+
+(defun rh-dired-ace-select-other-window ()
+  (interactive)
+  (let ((file-name (dired-get-file-for-visit)))
+    (when (ace-select-window)
+      (find-file file-name))))
+
+(defun rh-get-buffer-mode-window-list (&optional buffer-or-name mode all-frames)
+  (setq mode (or mode major-mode))
+  (let ((window-list '()))
+    (dolist (win (window-list-1 nil nil all-frames))
+      (when (eq
+             (with-selected-window win
+               major-mode)
+             mode)
+        (push win window-list)))
+    (nreverse window-list)))
+
+(defun rh-dired-select-next-dired-window ()
+  (interactive)
+  (let ((window-list (rh-get-buffer-mode-window-list)))
+    (if (> (length window-list) 1)
+        (select-window (nth 1 window-list))
+      (let ((directory (dired-current-directory))
+            (pos (point)))
+        (when (ace-select-window)
+          (find-file directory)
+          (goto-char pos))))))
+
+(defun rh-dired-alt-ace-select-other-window ()
+  (interactive)
+  (with-selected-window (frame-selected-window)
+    (let ((name (dired-get-file-for-visit)))
+      (when (ace-select-window)
+        (find-file name)))))
+
+(defun rh-dired-change-to-parent-dir ()
+  (interactive)
+  (dired-goto-file (concat (dired-current-directory) ".."))
+  (rh-dired-find-file))
+
+(defun rh-dired-open-file ()
+  "In dired, open the file named on this line."
+  (interactive)
+  (let ((file (dired-get-filename nil t)))
+    (message "Opening %s..." file)
+    (call-process "xdg-open" nil 0 nil file)))
+
+(setq dired-dwim-target t)
+
+(add-hook
+ 'dired-mode-hook
+ (lambda ()
+   (hl-line-mode 1)
+   (setq-local coding-system-for-read vr-dired-coding-system)
+   (setq-local find-file-visit-truename nil)
+   ;; (add-hook
+   ;;  'window-selection-change-functions
+   ;;  (lambda (win)
+   ;;    (when (eq (with-selected-window win major-mode) 'dired-mode)
+   ;;      (if (eq win (frame-selected-window))
+   ;;          (hl-line-mode 1)
+   ;;        (with-selected-window win
+   ;;          (when (= (length (get-buffer-window-list)) 1)
+   ;;            (hl-line-mode -1))))))
+   ;;  nil t)
+   ))
+
+(provide 'config-dired)
