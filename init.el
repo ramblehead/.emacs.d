fde474d3a2fde3e13d189a39a2f18ab6d4f5c945
ramblehead <v.rybynok@gmail.com>
2023-05-24 00:01:09 +0100

    Checkpoint

--- init.el
+++ init.el
@@ -220,6 +220,8 @@
   (setq tab-width 8)
   (setq fill-column 80)
 
+  (setq-default display-line-numbers-width 4)
+
   ;; Be quite
   (setq visible-bell t)
 
@@ -374,6 +376,84 @@
 
   :demand t)
 
+(defun configure-default-mode-line ()
+  (define-key-after
+    (lookup-key mode-line-column-line-number-mode-map
+                [mode-line down-mouse-1])
+    [total-lines-mode]
+    '(menu-item "Display Total Number of Lines" total-lines-mode
+		:help
+                "Toggle displaying a total number of lines in the mode-line"
+		:button (:toggle . total-lines-mode))
+    'line-number-mode)
+
+  (setq-default
+   mode-line-format
+   `((total-lines-mode
+      (:propertize
+       (:eval (format
+               (let ((width (max (length (number-to-string total-lines))
+                                 (or display-line-numbers-width 0))))
+                 (concat "%" (number-to-string width) "d"))
+               total-lines))
+       ;; face sml/col-number
+       help-echo ,(concat "Total lines mode\n"
+                          "mouse-1: Display Line "
+                          "and Column Mode Menu")
+       mouse-face mode-line-highlight
+       local-map ,mode-line-column-line-number-mode-map))
+
+     (total-lines-mode
+      (:propertize
+       " "
+       ;; face sml/numbers-separator
+       ))
+
+     (line-number-mode
+      (:propertize
+       "["
+       ;; face sml/numbers-separator
+       ))
+
+     (line-number-mode
+      (:propertize
+       (:eval (let ((width
+                     (if total-lines-mode
+                         (max (length (number-to-string total-lines))
+                              (or display-line-numbers-width 0))
+                       (or display-line-numbers-width 0))))
+                (concat "%" (number-to-string width) "l")))
+       ;; face sml/line-number
+       face mode-line-highlight
+       help-echo ,(concat "Line number mode\n"
+                          "mouse-1: Display Line "
+                          "and Column Mode Menu")
+       mouse-face mode-line-highlight
+       local-map ,mode-line-column-line-number-mode-map))
+
+     (column-number-mode
+      (:propertize
+       (:eval (if line-number-mode "," ""))
+       face sml/numbers-separator))
+
+     (column-number-mode
+      (:propertize
+       "%3c"
+       ;; 'face 'sml/col-number
+       help-echo ,(concat "Column number mode\n"
+                          "nmouse-1: Display Line "
+                          "and Column Mode Menu")
+       mouse-face 'mode-line-highlight
+       local-map ,mode-line-column-line-number-mode-map))
+
+     (line-number-mode
+      (:propertize
+       "]"
+       ;; 'face 'sml/numbers-separator
+       )))))
+
+;; (configure-default-mode-line)
+
 (defun configure-colour-themes ()
   (when (display-graphic-p)
     ;; (load-theme 'sanityinc-tomorrow-blue t)
@@ -495,62 +575,71 @@
   :hook (completion-list-mode . consult-preview-at-point-mode)
 
   ;; Replace bindings. Lazily loaded due by `use-package'.
-  :bind (;; C-c bindings in `mode-specific-map'
-         ("C-c M-x" . consult-mode-command)
-         ("C-c h" . consult-history)
-         ("C-c k" . consult-kmacro)
-         ("C-c m" . consult-man)
-         ("C-c i" . consult-info)
-         ([remap Info-search] . consult-info)
-         ;; C-x bindings in `ctl-x-map'
-         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
-         ;; Custom M-# bindings for fast register access
-         ("M-#" . consult-register-load)
-         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
-         ("C-M-#" . consult-register)
-         ;; Other custom bindings
-         ("M-<insert>" . consult-yank-pop)
-         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
-         ;; M-g bindings in `goto-map'
-         ("M-g e" . consult-compile-error)
-         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
-         ("M-g g" . consult-goto-line)             ;; orig. goto-line
-         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
-         ("M-g m" . consult-mark)
-         ("M-g k" . consult-global-mark)
-         ("M-g i" . consult-imenu)
-         ("M-g I" . consult-imenu-multi)
-         ;; M-s bindings in `search-map'
-         ("M-s d" . consult-find)
-         ;; ("M-s D" . consult-locate)
-         ("M-s g" . consult-grep)
-         ("M-s G" . consult-git-grep)
-         ("M-s r" . consult-ripgrep)
-         ("M-s l" . consult-line)
-         ("M-s L" . consult-line-multi)
-         ("M-s k" . consult-keep-lines)
-         ("M-s u" . consult-focus-lines)
-         ;; Isearch integration
-         ("M-s e" . consult-isearch-history)
-         :map isearch-mode-map
-         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
-         ;; Minibuffer history
-         :map minibuffer-local-map
-         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+  :bind
+  (;; C-c bindings in `mode-specific-map'
+   ("C-c M-x" . consult-mode-command)
+   ("C-c h" . consult-history)
+   ("C-c k" . consult-kmacro)
+   ("C-c m" . consult-man)
+   ("C-c i" . consult-info)
+   ([remap Info-search] . consult-info)
+   ;; C-x bindings in `ctl-x-map'
+   ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+   ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+   ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+   ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+   ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+   ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+   ;; Custom M-# bindings for fast register access
+   ("M-#" . consult-register-load)
+   ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+   ("C-M-#" . consult-register)
+   ;; Other custom bindings
+   ("M-<insert>" . consult-yank-pop)
+   ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+   ;; M-g bindings in `goto-map'
+   ("M-g e" . consult-compile-error)
+   ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+   ("M-g g" . consult-goto-line)             ;; orig. goto-line
+   ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+   ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+   ("M-g m" . consult-mark)
+   ("M-g k" . consult-global-mark)
+   ("M-g i" . consult-imenu)
+   ("M-g I" . consult-imenu-multi)
+   ;; M-s bindings in `search-map'
+   ("M-s d" . consult-find)
+   ;; ("M-s D" . consult-locate)
+   ("M-s g" . consult-grep)
+   ("M-s G" . consult-git-grep)
+   ("M-s r" . consult-ripgrep)
+   ("M-s l" . consult-line)
+   ("M-s L" . consult-line-multi)
+   ("M-s k" . consult-keep-lines)
+   ("M-s u" . consult-focus-lines)
+   ;; Isearch integration
+   ("M-s e" . consult-isearch-history)
+   :map isearch-mode-map
+   ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+   ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+   ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+   ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+   ;; Minibuffer history
+   :map minibuffer-local-map
+   ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+   ("M-r" . consult-history))                ;; orig. previous-matching-history-element
 
   :straight t
   :ensure t
   :demand t)
+
+(use-package total-lines
+  :config
+  (global-total-lines-mode 1)
+
+  :straight t
+  :demand t
+  :ensure t)
 
 (use-package rich-minority
   :config
