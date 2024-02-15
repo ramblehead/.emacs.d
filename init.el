676701884ca11839de58c3d8914092cea4178a51
ramblehead <v.rybynok@gmail.com>
2024-02-15 10:41:20 +0000

    Checkpoint

--- init.el
+++ init.el
@@ -706,6 +706,7 @@ when only symbol face names are needed."
          ("C-v" . yank)
          ("M-v" . yank-pop)
          ("C-z" . undo)
+         ("C-=" . text-scale-adjust)
          ;; Resize windows
          ("M-s-<up>" . enlarge-window)
          ("M-s-<kp-up>" . enlarge-window)
