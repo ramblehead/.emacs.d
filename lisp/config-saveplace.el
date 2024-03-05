57a9f9df0b585bc2f3c10980d9ee01d05a8535ce
ramblehead <v.rybynok@gmail.com>
2024-03-05 20:50:31 +0000

    Checkpoint

--- lisp/config-saveplace.el
+++ lisp/config-saveplace.el
@@ -1,11 +1,14 @@
 ;;; ramblehead's saveplace configuration
 
+(defun rh-recenter-after-find-file-timer-handler (buf)
+  (when (buffer-live-p buf)
+    (dolist (win (get-buffer-window-list buf nil t))
+      (with-selected-window win (recenter)))))
+
 (defun rh-recenter-after-find-file ()
   (run-with-timer
    0 nil
-   (lambda (buf)
-     (dolist (win (get-buffer-window-list buf nil t))
-       (with-selected-window win (recenter))))
+   #'rh-recenter-after-find-file-timer-handler
    (current-buffer)))
 
 (provide 'config-saveplace)
