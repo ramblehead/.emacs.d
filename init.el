db05db08139512ec8544a957f086f24628cf35b8
ramblehead <v.rybynok@gmail.com>
2024-03-11 12:50:31 +0000

    Checkpoint

--- init.el
+++ init.el
@@ -1825,6 +1825,20 @@ when only symbol face names are needed."
 
   :defer t)
 
+(use-package highlight-indent-guides
+  :config
+  (customize-set-value 'highlight-indent-guides-method 'character)
+  ;; (customize-set-value 'highlight-indent-guides-character ?\x2502)
+  (customize-set-value 'highlight-indent-guides-responsive 'top)
+
+  ;; (set-face-background 'highlight-indent-guides-odd-face "darkgray")
+  ;; (set-face-background 'highlight-indent-guides-even-face "dimgray")
+  ;; (set-face-foreground 'highlight-indent-guides-character-face "dimgray")
+
+  :straight t
+  :defer t
+  :ensure t)
+
 (use-package blacken
   :delight (blacken-mode " B")
   :straight t
