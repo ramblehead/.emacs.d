87235c0d1f8079c3ae0e3a04164f51330c0cd57f
ramblehead <v.rybynok@gmail.com>
2024-08-02 23:58:23 +0100

    Checkpoint

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(sanityinc-tomorrow-blue))
  '(custom-safe-themes
-   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "76ddb2e196c6ba8f380c23d169cf2c8f561fd2013ad54b987c516d3cabc00216" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
+   '("174712eeeee6f3f7dfff7702783b86c123cf81cee9ef2e23b26279311ec856b1" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "76ddb2e196c6ba8f380c23d169cf2c8f561fd2013ad54b987c516d3cabc00216" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(package-selected-packages '(gnu-elpa-keyring-update))
  '(safe-local-variable-values '((eval rh-project-setup))))
 (custom-set-faces
